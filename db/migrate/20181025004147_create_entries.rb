b187fd0ba0ad6f78481ef190a2d2e44574a0642a
Jared Murray <jared@agileblocks.io>
2018-10-24 20:44:42 -0400

    Added feed and entry models

---
+++ db/migrate/20181025004147_create_entries.rb
@@ -0,0 +1,15 @@
+class CreateEntries < ActiveRecord::Migration[5.2]
+  def change
+    create_table :entries do |t|
+      t.string :title
+      t.datetime :published
+      t.text :summary
+      t.text :url
+      t.string :author
+      t.text :image
+      t.integer :feed_id
+
+      t.timestamps
+    end
+  end
+end
