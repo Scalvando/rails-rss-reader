2ee7c2eaa619b7f14a12ac8459c44a99787f2337
Jared Murray <jared@agileblocks.io>
2018-10-25 16:49:29 -0400

    Fixed some styling, started adding sync, added saving and deleting articles

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,21 +1,47 @@
 # frozen_string_literal: true
 
 class EntriesController < ApplicationController
-  before_action :set_entry, only: [:show]
+  before_action :set_entry, only: [:save, :destroy]
 
   def index
-    feeds = current_user.feeds.all
+    @feeds = current_user.feeds.all
     @entries = []
-    feeds.each do |feed|
+    @feeds.each do |feed|
       @entries.concat(feed.entries.all.order('published desc'))
     end
 
     @entries.sort! { |entry_a, entry_b| entry_b.published <=> entry_a.published }
   end
 
+  def sync
+    if @feeds.each(&:sync)
+      redirect_to entries_path, notice: "Feeds updated"
+    end
+  end
+
+  def saved
+    @entries = current_user.entries.all.order('published desc')
+  end
+
+  def save
+    current_user.entries << @entry
+    flash[:success] = "Article successfully saved"
+    redirect_back fallback_location: root_path
+  end
+
+  def destroy
+    if current_user.entries.delete(@entry)
+      redirect_to saved_entries_path, notice: "Article successfully deleted"
+    end
+  end
+
   private
 
-  def set_entry
+  def set_saved_entry
     @entry = current_user.entries.find(params[:id])
+  end
+
+  def set_entry
+    @entry = Entry.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,22 @@ Rails.application.routes.draw do
   devise_for :admins, path: 'admin', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
 
+  root 'entries#index'
+
   resources :feeds do
     member do
       get :sync
+      get :saved
     end
   end
 
-  resources :entries
+  resources :entries do
+    member do
+      post :save
+    end
+    collection do
+      get :saved
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
