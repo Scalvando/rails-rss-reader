dbd55fbad90066debe6e03ef8bd20322f5760f39
Jared Murray <jared@agileblocks.io>
2018-10-25 23:30:19 -0400

    Added basic error handling and 404 page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,17 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  rescue_from ActionController::RoutingError, with: :error_render_method
+  # https://stackoverflow.com/questions/25841377/rescue-from-actioncontrollerroutingerror-in-rails-4
+  unless Rails.application.config.consider_all_requests_local
+    rescue_from ActionController::RoutingError, with: -> { render_404 }
+    rescue_from ActionController::UnknownController, with: -> { render_404 }
+    rescue_from ActiveRecord::RecordNotFound, with: -> { render_404 }
+  end
 
-  protect_from_forgery with: :exception
-
-  def error_render_method
-    respond_to do |type|
-      type.xml { render template: "errors/error_404", status: 404 }
-      type.all { render nothing: true, status: 404 }
+  def render_404
+    respond_to do |_type|
+      format.html { render template: "errors/not_found", status: 404 }
+      format.all { render nothing: true, status: 404 }
     end
     true
   end
--- config/routes.rb
+++ config/routes.rb
@@ -22,5 +22,6 @@ Rails.application.routes.draw do
       get :saved
     end
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '*path', to: 'errors#error_404', via: :all
 end
