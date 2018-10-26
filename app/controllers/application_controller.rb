6aa2471d8efaa7fe82a4360bd6e397956e544e13
Jared Murray <jared@agileblocks.io>
2018-10-25 22:57:28 -0400

    Started working on errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  rescue_from ActionController::RoutingError, with: :error_render_method
+
+  protect_from_forgery with: :exception
+
+  def error_render_method
+    respond_to do |type|
+      type.xml { render template: "errors/error_404", status: 404 }
+      type.all { render nothing: true, status: 404 }
+    end
+    true
+  end
 end
--- config/application.rb
+++ config/application.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require_relative 'boot'
 
 require "rails"
@@ -11,6 +13,7 @@ require "action_mailer/railtie"
 require "action_view/railtie"
 require "action_cable/engine"
 require "sprockets/railtie"
+require ""
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
