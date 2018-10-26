18289ba76ef9dc47cd194c1d0b48394ea1b674fb
Jared Murray <jared@agileblocks.io>
2018-10-26 12:41:02 -0400

    Changed first_or_initialize to look for URL of entry rather than title

--- app/models/feed.rb
+++ app/models/feed.rb
@@ -11,15 +11,16 @@ class Feed < ApplicationRecord
   has_many :users, through: :user_feeds
 
   def sync
+    # https://www.sitepoint.com/building-an-rss-reader-in-rails-is-easy/
     content = Feedjira::Feed.fetch_and_parse(url)
 
     content.entries.each do |entry|
-      local_entry = entries.where(title: entry.title).first_or_initialize
+      local_entry = entries.where(url: entry.url).first_or_initialize
       local_entry.update_attributes(
+        title: entry.title,
         image: entry.image,
         summary: entry.summary,
         author: entry.author,
-        url: entry.url,
         published: entry.published
       )
     end
