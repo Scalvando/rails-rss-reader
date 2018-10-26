17f1c7b3d8e4cf556a184fd0488609890e48b41f
Jared Murray <jared@agileblocks.io>
2018-10-25 23:54:52 -0400

    Added seed data

--- db/migrate/20181025005347_create_users_feeds_join_table.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class CreateUsersFeedsJoinTable < ActiveRecord::Migration[5.2]
-  create_join_table :users, :feeds do |t|
-    t.index :user_id
-    t.index :feed_id
-  end
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+require "feedjira"
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +8,53 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!([
+  { email: 'demo@test.com', password: 'secret123', password_confirmation: 'secret123' },
+  { email: 'test@test.com', password: 'secret1234', password_confirmation: 'secret1234' },
+])
+
+puts 'Users creates'
+
+feed_urls = ['http://feeds.bbci.co.uk/news/world/rss.xml', 'http://feeds.reuters.com/Reuters/worldNews']
+
+feed_urls.each do |feed_url|
+  content = Feedjira::Feed.fetch_and_parse(feed_url)
+
+  feed = Feed.new(
+    url: feed_url,
+    title: content.title,
+    description: content.description
+  )
+
+  if feed.save
+    puts "Feed for #{feed_url} created"
+  end
+
+  if feed.sync
+    puts "Feed successfully synced"
+  end
+end
+
+user_demo = User.find_by_email('demo@test.com')
+
+user_test = User.find_by_email('test@test.com')
+
+user_demo.feeds << Feed.find_by_url(feed_urls[0])
+
+puts 'Added feeds to user demo'
+
+user_test.feeds << Feed.all
+
+puts 'Added feeds to user demo'
+
+user_demo.feeds.all.each do |user_feed|
+  user_demo.entries << user_feed.entries.all.limit(3)
+end
+
+puts 'Added saved entries to user demo'
+
+user_test.feeds.all.each do |user_feed|
+  user_test.entries << user_feed.entries.all.limit(2)
+end
+
+puts 'Added saved entries to user test'
